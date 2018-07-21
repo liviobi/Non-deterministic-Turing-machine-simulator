c532f7bd5e23c6fcc10dff547d43d8c5cb6dda54
liviobi <liviobiondo@gmail.com>
2018-07-22 00:32:13 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/FSM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FSM.dir/main.c.o"
+  "FSM.pdb"
+  "FSM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
