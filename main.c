20cf9dfc69e03027058b7e6c3be61d8e87dd62a6
liviobi <liviobiondo@gmail.com>
2018-07-24 18:26:44 +0200

    can read the whole file

--- main.c
+++ main.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//todo check for empty inputs
 int main() {
-    int a;
-    int b;
-    if(scanf("%d %d", &a, &b) > 0){
-        int c = a + b;
-        printf("%d",c);
+    char strings[100];
+
+    char command[3];
+    int state;
+    char red;
+    char written;
+    char headShift;
+    int nextState;
+    //scan tr
+    scanf("%s", command);
+    printf("%s\n", command);
+    
+    while(scanf("%d %c %c %c %d",&state,&red,&written,&headShift,&nextState) == 5){
+        printf("%d %c %c %c %d\n",state,red,written,headShift,nextState);
+        //todo setup the fsm
     }
-    return 0;
+    
+    //scan acc
+    scanf("%s",command);
+    printf("%s\n", command);
+
+    int acceptedState;
+    while(scanf("%d", &acceptedState) == 1){
+        printf("%d\n",acceptedState);
+    }
+
+    //scan max
+    scanf("%s",command);
+    printf("%s\n", command);
+
+    int maxIterations; //todo see for long or double, make static
+    scanf("%d",&maxIterations);
+    printf("%d\n",maxIterations);
+
+    //scan r
+    scanf("%s",command);
+    printf("%s\n", command);
+    int i = 15;
+    int j;
+    while(scanf("%s",strings)==1 && i>0){ //todo make safer
+        printf("%s\n",strings);
+        i--;
+    }
+
 }
