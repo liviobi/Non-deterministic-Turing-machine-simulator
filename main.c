af1d9c75cd6409a37728ee8085cbc8d239fd4c71
liviobi <liviobiondo@gmail.com>
2018-07-28 19:51:34 +0200

    corrlctly setting up the nodes except final states

--- main.c
+++ main.c
@@ -1,47 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FALSE 0
+#define TRUE 1
+#define STARTINLEN 50
+#define CHARACTERS 27
+#define NEXTSTATE
+
+typedef struct transition_s{
+    char written;
+    char headShift;
+    int  nextState;
+    struct transition_s*nextTransition;
+}transition;
+
+typedef struct state_s {
+    int acceptState;
+    transition *transitions[CHARACTERS];
+} state;
+
+int maxTransitions; //todo see for long or double
+state *states[STARTINLEN]; //todo non funziona se gli stati hanno numeri grandi
+
+void setMaxTransitions();
+
+void setAcceptedStates();
+
+void setupStates();
+
 //todo check for empty inputs
 int main() {
     char strings[100];
 
+    setupStates();
+    setAcceptedStates();
+    setMaxTransitions();
+    //scan r
     char command[3];
-    int state;
+    scanf("%s",command);
+    printf("%s\n", command);
+    while(scanf("%s",strings)==1){ //todo make safer
+        printf("%s\n",strings);
+    }
+
+    //prova
+    for(int i =0; i<STARTINLEN; i++){
+        if (states[i] != NULL){
+            printf("%d\n",i);
+            for(int transitionCharIndex = 0; transitionCharIndex<CHARACTERS;transitionCharIndex++){
+                transition *transitionToPrint = states[i]->transitions[transitionCharIndex];
+                while (transitionToPrint != NULL){
+                    char transitionChar = (char)transitionCharIndex + 95;
+                    printf("%c ",transitionChar);
+                    printf("%c %c %d \n",transitionToPrint->written,transitionToPrint->headShift,transitionToPrint->nextState);
+                    transitionToPrint = transitionToPrint->nextTransition;
+
+                }
+
+            }
+        }
+    }
+
+}
+
+void setupStates() {
+    char command[3];
+    int currentState;
     char red;
     char written;
     char headShift;
     int nextState;
+
     //scan tr
     scanf("%s", command);
     printf("%s\n", command);
-    while(scanf("%d %c %c %c %d",&state,&red,&written,&headShift,&nextState) == 5){
-        printf("%d %c %c %c %d\n",state,red,written,headShift,nextState);
+    while(scanf("%d %c %c %c %d",&currentState,&red,&written,&headShift,&nextState) == 5){
+        printf("%d %c %c %c %d\n",currentState,red,written,headShift,nextState);
+        if(states[currentState]== NULL){
+            state* newState = (state *)malloc(sizeof(state));
+            states[currentState] = newState;
+            newState->acceptState = FALSE;
+            transition* newTransition = (transition *)malloc(sizeof(transition));
+            newTransition->nextState = nextState;
+            newTransition->written = written;
+            newTransition->headShift = headShift;
+            newTransition->nextTransition = NULL;
+            newState->transitions[red-95] = newTransition;
+
+        }else{
+            transition* newTransition = (transition *)malloc(sizeof(transition));
+            newTransition->nextState = nextState;
+            newTransition->written = written;
+            newTransition->headShift = headShift;
+            newTransition->nextTransition = states[currentState]->transitions[red-95];
+            states[currentState]->transitions[red-95] = newTransition;
+
+
+        }
+
         //todo setup the fsm
     }
-    
-    //scan acc
+    for(int i = 0; i<STARTINLEN;i++){
+        if (states[i]==NULL){
+        }
+    }
+state states[STARTINLEN] = {NULL};
+}
+
+void setAcceptedStates() {//scan acc
+    char command[3];
     scanf("%s",command);
     printf("%s\n", command);
     int acceptedState;
     while(scanf("%d", &acceptedState) == 1){
         printf("%d\n",acceptedState);
     }
+}
 
-    //scan max
+void setMaxTransitions() {//scan max
+    char command[3];
     scanf("%s",command);
     printf("%s\n", command);
-    int maxTransitions; //todo see for long or double, make static
     scanf("%d",&maxTransitions);
     printf("%d\n",maxTransitions);
-
-    //scan r
-    scanf("%s",command);
-    printf("%s\n", command);
-    int i = 15;
-    int j;
-    while(scanf("%s",strings)==1 && i>0){ //todo make safer
-        printf("%s\n",strings);
-        i--;
-    }
-
 }
